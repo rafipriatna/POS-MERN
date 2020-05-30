9b138bbb73ee4f2af095f727477e06c0acfe3ccc
Rafi Priatna <stormrider136@gmail.com>
2020-05-30 20:35:11 +0700

    added description

--- server/server.js
+++ server/server.js
@@ -4,4 +4,6 @@ const app = require('./app')
 const port = process.env.PORT || 5000
 const server = http.createServer(app)
 
-server.listen(port)
+server.listen(port, () => {
+    console.log(`Server berjalan di port ${port}`)
+})
