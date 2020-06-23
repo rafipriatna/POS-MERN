f8f1a5b38b84ea8ffe11ff5108df7edd5f273fae
Rafi Priatna <stormrider136@gmail.com>
2020-06-23 15:33:06 +0700

    fix auth

--- server/Api/Middleware/checkAuth.js
+++ server/Api/Middleware/checkAuth.js
@@ -23,13 +23,7 @@ exports.userAuth = (req, res, next) => {
     const token = req.headers.authorization.split(" ")[1]; // Split Bearer header
     const decoded = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
     if (decoded.level === 0 || decoded.level === 1) {
-      if (decoded.id != req.params.id) {
-        res.status(401).json({
-          error: "Auth gagal",
-        });
-      } else {
-        next();
-      }
+      next();
     } else {
       return res.status(401).json({
         error: "Auth gagal",
