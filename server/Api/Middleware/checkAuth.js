d873858e92f1a0136999ab518086a11b8d762c97
Rafi Priatna <stormrider136@gmail.com>
2020-06-23 13:58:48 +0700

    prettier

--- server/Api/Middleware/checkAuth.js
+++ server/Api/Middleware/checkAuth.js
@@ -1,59 +1,43 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 exports.adminAuth = (req, res, next) => {
-    try {
-        const token = req
-            .headers
-            .authorization
-            .split(" ")[1] // Split Bearer header
-        const decoded = jwt.verify(token, process.env.TOKEN_SECRET_KEY)
-        if (decoded.level === 0) {
-            next()
-        } else {
-            return res
-                .status(401)
-                .json({
-                    error: 'Auth gagal'
-                })
-        }
-    } catch (error) {
-        return res
-            .status(401)
-            .json({
-                error: 'Auth gagal'
-            })
+  try {
+    const token = req.headers.authorization.split(" ")[1]; // Split Bearer header
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
+    if (decoded.level === 0) {
+      next();
+    } else {
+      return res.status(401).json({
+        error: "Auth gagal",
+      });
     }
-}
+  } catch (error) {
+    return res.status(401).json({
+      error: "Auth gagal",
+    });
+  }
+};
 
 exports.userAuth = (req, res, next) => {
-    try {
-        const token = req
-            .headers
-            .authorization
-            .split(" ")[1] // Split Bearer header
-        const decoded = jwt.verify(token, process.env.TOKEN_SECRET_KEY)
-        if (decoded.level === 0 || decoded.level === 1) {
-            if (decoded.id != req.params.id){
-                res
-                .status(401)
-                .json({
-                    error: 'Auth gagal'
-                })
-            }else{
-                next()
-            }
-        } else {
-            return res
-                .status(401)
-                .json({
-                    error: 'Auth gagal'
-                })
-        }
-    } catch (error) {
-        return res
-            .status(401)
-            .json({
-                error: 'Auth gagal'
-            })
+  try {
+    const token = req.headers.authorization.split(" ")[1]; // Split Bearer header
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
+    if (decoded.level === 0 || decoded.level === 1) {
+      if (decoded.id != req.params.id) {
+        res.status(401).json({
+          error: "Auth gagal",
+        });
+      } else {
+        next();
+      }
+    } else {
+      return res.status(401).json({
+        error: "Auth gagal",
+      });
     }
-}
+  } catch (error) {
+    return res.status(401).json({
+      error: "Auth gagal",
+    });
+  }
+};
